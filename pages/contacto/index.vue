c63bf5519f19ae0133730545e3a4d4081427368b
Mauricio Garay <mgaray@urgencias.local>
2020-03-18 22:33:17 -0300

    first commit

---
+++ pages/contacto/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <h1>Pages Contacto</h1>
+  </div>
+</template>
---
+++ pages/nosotros.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <h1>Pages Nosotros</h1>
+  </div>
+</template>
